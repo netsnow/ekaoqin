f33a523189833ca62caec13195b00c914af1f7b8
baixue <netsnow@users.noreply.github.com>
2018-10-10 17:48:56 +0800

    service add

---
+++ ekaoqin-java/src/main/java/org/snow/service/impl/StudentServiceImpl.java
@@ -0,0 +1,56 @@
+package org.snow.service.impl;
+
+
+import org.snow.dao.jpa.RoomRepository;
+import org.snow.dao.jpa.StudentRepository;
+import org.snow.model.business.Room;
+import org.snow.model.business.Student;
+import org.snow.service.RoomService;
+import org.snow.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service("studentService")
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public List<Student> getAllStudents() {
+
+        Iterable<Student> geted = studentRepository.findAll();
+        List<Student> list = new ArrayList<Student>();
+        geted.forEach(single -> {
+            if (!single.getIsDeleted()) {
+                list.add(single);
+            }
+        });
+        return list;
+    }
+
+    @Override
+    public Boolean addStudent(Student student) {
+        studentRepository.save(student);
+        return true;
+    }
+
+    @Override
+    public Boolean updateStudentById(Long studentId, Student student) {
+        student.setId(studentId);
+        studentRepository.save(student);
+        return true;
+    }
+
+    @Override
+    public Boolean deleteStudentById(Long studentId) {
+        Optional<Student> student = studentRepository.findById(studentId);
+        student.get().setIsDeleted(true);
+        studentRepository.save(student.get());
+        return true;
+    }
+}
