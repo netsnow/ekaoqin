8882c20a08184347b881e2423d30b6e1c75dc888
baixue <netsnow@users.noreply.github.com>
2018-10-18 15:45:46 +0800

    entry fix

--- ekaoqin-java/src/main/java/org/snow/mq/MqLinstener.java
+++ ekaoqin-java/src/main/java/org/snow/mq/MqLinstener.java
@@ -1,12 +1,29 @@
 package org.snow.mq;
 
+import com.alibaba.fastjson.JSONObject;
+import org.snow.model.business.EntryLog;
+import org.snow.service.EntryLogService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MqLinstener {
-    @RabbitListener(queues="amq_sync_xhz")    //监听器监听指定的Queue
-    public void processC(String str) {
-        System.out.println("Receive:"+str);
+
+    @Autowired
+    private EntryLogService entryLogService;
+
+    @RabbitListener(queues = "amq_sync_xhz")    //监听器监听指定的Queue
+    public void addEntryLog(String entryMessage) {
+        System.out.println("Receive:" + entryMessage);
+        JSONObject entryMessageJson = JSONObject.parseObject(entryMessage);
+        //当取得数据的类型为普通抓拍记录（0）时
+        if (entryMessageJson.get("dataType").equals("0")) {
+            EntryLog entryLog = new EntryLog();
+            entryLog.setFaceSysUserId(entryMessageJson.get("cardId").toString());
+            entryLog.setCameraId(entryMessageJson.get("cameraInfo").toString());
+            entryLogService.addEntryLog(entryLog);
+        }
+
     }
 }
